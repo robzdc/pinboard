e9f730d803f814e636d2a4a6a615d3694532ee27
Roberto Del Castillo <robz.del.castillo@gmail.com>
2014-05-23 11:57:12 -0500

    Added active_admin

---
+++ app/admin/pin.rb
@@ -0,0 +1,17 @@
+ActiveAdmin.register Pin do
+
+  
+  # See permitted parameters documentation:
+  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  permit_params :title, :description, :status, :user_id, :category_id, :type_id
+  #
+  # or
+  #
+  # permit_params do
+  #  permitted = [:permitted, :attributes]
+  #  permitted << :other if resource.something?
+  #  permitted
+  # end
+  
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,40 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140512180816) do
+ActiveRecord::Schema.define(version: 20140522225055) do
+
+  create_table "active_admin_comments", force: true do |t|
+    t.string   "namespace"
+    t.text     "body"
+    t.string   "resource_id",   null: false
+    t.string   "resource_type", null: false
+    t.integer  "author_id"
+    t.string   "author_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "active_admin_comments", ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id", using: :btree
+  add_index "active_admin_comments", ["namespace"], name: "index_active_admin_comments_on_namespace", using: :btree
+  add_index "active_admin_comments", ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id", using: :btree
+
+  create_table "admin_users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
+  add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "identities", force: true do |t|
     t.integer  "user_id"
@@ -22,6 +55,28 @@ ActiveRecord::Schema.define(version: 20140512180816) do
   end
 
   add_index "identities", ["user_id"], name: "index_identities_on_user_id", using: :btree
+
+  create_table "pins", force: true do |t|
+    t.string   "title",       limit: 70
+    t.text     "description"
+    t.integer  "status",      limit: 1,  default: 2
+    t.integer  "user_id",                            null: false
+    t.integer  "category_id",                        null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "type_id",                            null: false
+  end
+
+  add_index "pins", ["category_id"], name: "index_pins_on_category_id", using: :btree
+  add_index "pins", ["type_id"], name: "index_pins_on_type_id", using: :btree
+  add_index "pins", ["user_id"], name: "index_pins_on_user_id", using: :btree
+
+  create_table "types", force: true do |t|
+    t.string   "name",        null: false
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
